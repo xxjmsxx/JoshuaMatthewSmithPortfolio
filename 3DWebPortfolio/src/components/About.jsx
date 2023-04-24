ddb95cc1c283b88bf9a5d916ab6636977acd1ca4
Joshua Smith <sarjsmithde@gmail.com>
2023-04-24 13:15:27 +0200

    added SectionWrapper to center items and pass idName

--- 3DWebPortfolio/src/components/About.jsx
+++ 3DWebPortfolio/src/components/About.jsx
@@ -6,6 +6,8 @@ import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
+import { SectionWrapper } from '../hoc';
+
 const ServiceCard = ({ title, icon, index }) => {
   return (
     <Tilt className='xs:w-[250px] w-full'>
@@ -67,4 +69,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
