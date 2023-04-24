ffba461f8c978583cef6e900dc32fa4b13557dd4
Joshua Smith <sarjsmithde@gmail.com>
2023-04-24 12:51:56 +0200

    Service cards. Learn how to make the background of them 3D/infinite mirror

--- 3DWebPortfolio/src/components/canvas/Computers.jsx
+++ 3DWebPortfolio/src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
 const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
@@ -24,8 +24,8 @@ const computer = useGLTF('./desktop_pc/scene.gltf')
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={ isMobile ? 0.6 : 0.75}
+        position={ isMobile? [0, -3, -2] :[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -33,6 +33,30 @@ const computer = useGLTF('./desktop_pc/scene.gltf')
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(
+      '(max-width: 500px)');
+
+      setIsMobile(mediaQuery.matches);
+
+      const handleMediaQueryChange = (e) => {
+        setIsMobile(e.matches);
+      }
+
+      mediaQuery.addEventListener('change',
+      handleMediaQueryChange);
+
+      return () => {
+        mediaQuery.removeEventListener(
+          'change',
+          handleMediaQueryChange
+        )
+      }
+  }, [])
+
+
   return (
     <Canvas
     frameLoop='demand'
@@ -47,7 +71,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
