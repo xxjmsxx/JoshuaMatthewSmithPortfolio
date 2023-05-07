8a0e4ac3846e706aecdf56c4c23c4a7fedf3ad4a
Joshua Smith <sarjsmithde@gmail.com>
2023-05-08 00:30:35 +0200

    css for text in experience cards changed

--- src/assets/index.js
+++ src/assets/index.js
@@ -23,6 +23,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+
 import construction from "./construction.png";
 
 import lewagon from "./company/lewagon.png";
